394e5e55089072507483fd9b557af1f2e39d8236
Favour <favobuchi@outlook.com>
2023-05-09 13:46:36 +0100

    some formalities

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+/**
+ * Renders the React application and mounts it to the root element of the HTML document.
+ * @param {JSX.Element} component - The root component of the React application.
+ * @param {HTMLElement} rootElement - The root element in the HTML document to mount the application.
+ */
+ReactDOM.render(<App/>, document.getElementById("root"));
+
